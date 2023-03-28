6e84674295ba026882cb0892c8b6fae6b39d8da6
profemontero <profegmontero@gmail.com>
2023-03-28 12:59:08 +0200

    first commit

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");  
+        
+        
+    }
+    
+    
+}
---
+++ src/holamundo/Programa1.java
@@ -0,0 +1,11 @@
+
+package holamundo;
+
+public class Programa1 {
+    public static void main(String[] args) {
+    byte edad = 15;
+    System.out.println("la edad es: "+edad);
+  }
+        
+}
+
