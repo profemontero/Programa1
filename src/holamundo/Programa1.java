2fd06043f4608f78fa9b380613ab8defd8d29673
profemontero <profegmontero@gmail.com>
2023-03-30 07:29:48 +0200

    casting en Java

--- src/holamundo/Programa1.java
+++ src/holamundo/Programa1.java
@@ -1,11 +1,29 @@
-
 package holamundo;
 
 public class Programa1 {
-    public static void main(String[] args) {
-    byte edad = 15;
-    System.out.println("la edad es: "+edad);
-  }
-        
-}
 
+    public static void main(String[] args) {
+
+        double r = 4.5;
+        int x = (int) r; //casting
+
+        System.out.println("r = " + r);
+        System.out.println("x = " + x);
+
+
+//        byte b = 64;
+//        //char n = b;
+//        char n= (char) b;
+//        
+//        System.out.println("b = " + b);
+//        System.out.println("n = " + n);
+
+
+//        float r = 3.4f;
+//        //long l = r;  //error
+//        long l = (long) r;
+//        System.out.println("r = " + r);
+//        System.out.println("l = " + l);
+
+    }
+}
